0abfd1669b5715c414b6a2c6eea96419e08eb003
Ty Valencia <tyvalencia@gmail.com>
2024-04-08 20:35:40 -0700

    Current work on analyzer

--- src/analyzer.js
+++ src/analyzer.js
@@ -1,8 +1,4 @@
-// The semantic analyzer exports a single function, analyze(match), that
-// accepts a grammar match object (the CST) from Ohm and produces the
-// internal representation of the program (pretty close to what is usually
-// called the AST). This representation also includes entities from the
-// standard library, as needed.
+// Semantic Analyzer skeleton from Dr. Toal's Carlos project
 
 import * as core from "./core.js"
 
@@ -32,7 +28,6 @@ class Context {
   }
 }
 
-// TODO: implement analyzer functions relating to tab indentation and unique features to BuildLang
 export default function analyze(match) {
   let context = Context.root()
 
@@ -199,9 +194,6 @@ export default function analyze(match) {
   }
 
 
-
-
-
   // Builder taken from Carlos, adapt function bodies to match correct parameters
 
   const builder = match.matcher.grammar.createSemantics().addOperation("rep", {
@@ -209,13 +201,18 @@ export default function analyze(match) {
       return core.program(statements.children.map(s => s.rep()))
     },
 
-    VarDecl(modifier, id, _eq, exp) {
+    VarDecl(baseType, id, _eq, exp) {
+      const type = baseType.rep() // Implement baseType
       const initializer = exp.rep()
-      const readOnly = modifier.sourceString === "const"
       const variable = core.variable(id.sourceString, readOnly, initializer.type)
       mustNotAlreadyBeDeclared(id.sourceString, { at: id })
       context.add(id.sourceString, variable)
       return core.variableDeclaration(variable, initializer)
+    },
+
+    Type(modifier, baseType) {
+      const readOnly = modifier.sourceString === "const"
+      return core.typeDeclaration(baseType.rep(), readOnly)
     },
 
     FunDecl(async, _block, id, _open, paramList, _close, _sends, typeArray, _colon, stmtBlock) {
@@ -230,31 +227,317 @@ export default function analyze(match) {
       const returnType = type.children?.[0]?.rep() ?? VOID
       fun.type = core.functionType(paramTypes, returnType)
 
-      const body = block.rep()
+      const body = stmtBlock.rep()
 
       context = context.parent
       return core.functionDeclaration(fun, params, body)
     },
 
-    Assignment(lhs, _eq, rhs) {
-      const left = lhs.rep()
-      const right = rhs.rep()
-      mustBeAssignable(right, left, { at: lhs })
-      mustNotBeReadOnly(left, { at: lhs })
-      return core.assignment(left, right)
+    Param(typeArray, id) {
+      const type = typeArray.rep()
+      const param = core.variable(id.sourceString, false, type)
+      context.add(id.sourceString, param)
+      return param
     },
 
-    TypeArray(_left, baseType, _right) {
-      return core.arrayType(baseType.rep())
+    TypeArray(_lhb, type, _rhb) {
+      return type.rep()
     },
 
-    // Rest of program rep
-    // ...
+    Statement_assignment(idList, _eq, expList) {
+      const ids = idList.children.map(id => id.sourceString)
+      const exps = expList.children.map(exp => exp.rep())
+      const variables = ids.map(id => context.lookup(id))
+      const assignments = variables.map((variable, i) => core.assignment(variable, exps[i]))
+      return core.sequence(assignments)
+    },
 
+    Statement_bump(exp, operator) {
+      const variable = exp.rep()
+      mustHaveIntegerType(variable, { at: exp })
+      return operator.sourceString === "++"
+        ? core.increment(variable)
+        : core.decrement(variable)
+    },
 
+    Statement_break(breakKeyword) {
+      mustBeInLoop({ at: breakKeyword })
+      return core.breakStatement
+    },
 
+    Statement_send(returnKeyword, exp) {
+      mustBeInAFunction({ at: returnKeyword })
+      mustReturnSomething(context.function, { at: returnKeyword })
+      const returnExpression = exp.rep()
+      mustBeReturnable(returnExpression, { from: context.function }, { at: exp })
+      return core.returnStatement(returnExpression)
+    },
 
+    Statement_shortsend(returnKeyword) {
+      mustBeInAFunction({ at: returnKeyword })
+      mustNotReturnAnything(context.function, { at: returnKeyword })
+      return core.shortReturnStatement()
+    },
 
+    Say(say, _open, args, _close) {
+      const exp = args.rep()
+      const f = context.function
+      mustBeInAFunction({ at: say })
+      mustReturnSomething(f, { at: say })
+      mustBeReturnable(exp, { from: f }, { at: say })
+      return core.say(exp)
+    },
+
+    FunCall(id, _open, args, _close) {
+      const fun = context.lookup(id.sourceString)
+      const exps = args.rep()
+      mustBeCallable(fun, { at: id })
+      mustHaveCorrectArgumentCount(exps.length, fun.type.paramTypes.length, { at: id })
+      exps.forEach((exp, i) => mustBeAssignable(exp, { toType: fun.type.paramTypes[i] }, { at: args }))
+      return core.funCall(fun, exps)
+    },
+
+    Args(argsList) {
+      return argsList.rep()
+    },
+
+    IfStmt_long(_if, exp, _colon1, block1, _else, _colon2, block2) {
+      const test = exp.rep()
+      mustHaveBooleanType(test, { at: exp })
+      context = context.newChildContext()
+      const consequent = block1.rep()
+      context = context.parent
+      context = context.newChildContext()
+      const alternate = block2.rep()
+      context = context.parent
+      return core.ifStatement(test, consequent, alternate)
+    },
+
+    IfStmt_elsif(_if, exp, _colon, block, _else, trailingIfStatement) {
+      const test = exp.rep()
+      mustHaveBooleanType(test, { at: exp })
+      context = context.newChildContext()
+      const consequent = block.rep()
+      const alternate = trailingIfStatement.rep()
+      return core.ifStatement(test, consequent, alternate)
+    },
+
+    IfStmt_short(_if, exp, _colon, block) {
+      const test = exp.rep()
+      mustHaveBooleanType(test, { at: exp })
+      context = context.newChildContext()
+      const consequent = block.rep()
+      context = context.parent
+      return core.shortIfStatement(test, consequent)
+    },
+
+    LoopStmt_while(_while, exp, _colon, block) {
+      const test = exp.rep()
+      mustHaveBooleanType(test, { at: exp })
+      context = context.newChildContext({ inLoop: true })
+      const body = block.rep()
+      context = context.parent
+      return core.whileStatement(test, body)
+    },
+
+    LoopStmt_repeat(_stack, intlit, _colon, block) {
+      const count = intlit.rep()
+      mustHaveIntegerType(count, { at: intlit })
+      context = context.newChildContext({ inLoop: true })
+      const body = block.rep()
+      context = context.parent
+      return core.repeatStatement(count, body)
+    },
+
+    LoopStmt_range(_for, id, _in, exp1, op, exp2, _colon, block) {
+      const [low, high] = [exp1.rep(), exp2.rep()]
+      mustHaveIntegerType(low, { at: exp1 })
+      mustHaveIntegerType(high, { at: exp2 })
+      const iterator = core.variable(id.sourceString, INT, true)
+      context = context.newChildContext({ inLoop: true })
+      context.add(id.sourceString, iterator)
+      const body = block.rep()
+      context = context.parent
+      return core.forRangeStatement(iterator, low, op.sourceString, high, body)
+    },
+
+    LoopStmt_collection(_for, id, _in, exp, _colon, block) {
+      const collection = exp.rep()
+      mustHaveAnArrayType(collection, { at: exp })
+      const iterator = core.variable(id.sourceString, true, collection.type.baseType)
+      context = context.newChildContext({ inLoop: true })
+      context.add(iterator.name, iterator)
+      const body = block.rep()
+      context = context.parent
+      return core.forStatement(iterator, collection, body)
+    },
+
+    StmtBlock(_indent, Stmt, _dedent) {
+      return Stmt.rep()
+    },
+
+    
+    // Expressions mostly have correct bodies
+
+    Exp_or(exp, _ops, exps) {
+      let left = exp.rep()
+      mustHaveBooleanType(left, { at: exp })
+      for (let e of exps.children) {
+        let right = e.rep()
+        mustHaveBooleanType(right, { at: e })
+        left = core.binary("||", left, right, BOOLEAN)
+      }
+      return left
+    },
+
+    Exp_and(exp, _ops, exps) {
+      let left = exp.rep()
+      mustHaveBooleanType(left, { at: exp })
+      for (let e of exps.children) {
+        let right = e.rep()
+        mustHaveBooleanType(right, { at: e })
+        left = core.binary("&&", left, right, BOOLEAN)
+      }
+      return left
+    },
+
+    Exp1_compare(exp1, relop, exp2) {
+      const [left, op, right] = [exp1.rep(), relop.sourceString, exp2.rep()]
+      if (["<", "<=", ">", ">="].includes(op)) {
+        mustHaveNumericOrStringType(left, { at: exp1 })
+      }
+      mustBothHaveTheSameType(left, right, { at: relop })
+      return core.binary(op, left, right, BOOLEAN)
+    },
+
+    Exp2_add(exp1, addOp, exp2) {
+      const [left, op, right] = [exp1.rep(), addOp.sourceString, exp2.rep()]
+      if (op === "+") {
+        mustHaveNumericOrStringType(left, { at: exp1 })
+      } else {
+        mustHaveNumericType(left, { at: exp1 })
+      }
+      mustBothHaveTheSameType(left, right, { at: addOp })
+      return core.binary(op, left, right, left.type)
+    },
+
+    Exp3_multiply(exp1, mulOp, exp2) {
+      const [left, op, right] = [exp1.rep(), mulOp.sourceString, exp2.rep()]
+      mustHaveNumericType(left, { at: exp1 })
+      mustBothHaveTheSameType(left, right, { at: mulOp })
+      return core.binary(op, left, right, left.type)
+    },
+
+    Exp4_power(exp1, powerOp, exp2) {
+        const [left, op, right] = [exp1.rep(), powerOp.sourceString, exp2.rep()]
+        mustHaveNumericType(left, { at: exp1 })
+        mustBothHaveTheSameType(left, right, { at: powerOp })
+        return core.binary(op, left, right, left.type)
+      },
+
+    Exp4_negation(_neg, exp) { 
+      const operand = exp.rep()
+      mustHaveNumericType(operand, { at: exp })
+      return core.unary("-", operand, operand.type)
+    },
+
+    Left_pipe(exp1, _pipe, exp2) { 
+      const [left, right] = [exp1.rep(), exp2.rep()]
+      return core.pipeForward(left, right)
+    },
+
+    Right_pipe(exp1, _pipe, exp2) {
+      const [left, right] = [exp1.rep(), exp2.rep()]
+      return core.pipeBackward(left, right)
+    },
+
+    // Primary taken from Carlos' Exp9, should follow exact same format
+    Primary_emptyarray(ty, _open, _close) {
+      const type = ty.rep()
+      mustBeAnArrayType(type, { at: ty })
+      return core.emptyArray(type)
+    },
+
+    Primary_arrayexp(_open, args, _close) {
+      const elements = args.asIteration().children.map(e => e.rep())
+      mustAllHaveSameType(elements, { at: args })
+      return core.arrayExpression(elements)
+    },
+
+    Primary_emptyopt(_no, type) {
+      return core.emptyOptional(type.rep())
+    },
+
+    Primary_parens(_open, expression, _close) {
+      return expression.rep()
+    },
+
+    Primary_subscript(exp1, _open, exp2, _close) {
+      const [array, subscript] = [exp1.rep(), exp2.rep()]
+      mustHaveAnArrayType(array, { at: exp1 })
+      mustHaveIntegerType(subscript, { at: exp2 })
+      return core.subscript(array, subscript)
+    },
+
+    Primary_member(exp, dot, id) {
+      const object = exp.rep()
+      let structType
+      if (dot.sourceString === "?.") {
+        mustHaveAnOptionalStructType(object, { at: exp })
+        structType = object.type.baseType
+      } else {
+        mustHaveAStructType(object, { at: exp })
+        structType = object.type
+      }
+      mustHaveMember(structType, id.sourceString, { at: id })
+      const field = structType.fields.find(f => f.name === id.sourceString)
+      return core.memberExpression(object, dot.sourceString, field)
+    },
+
+    Primary_call(exp, open, expList, _close) {
+      const callee = exp.rep()
+      mustBeCallable(callee, { at: exp })
+      const exps = expList.asIteration().children
+      const targetTypes =
+        callee?.kind === "StructType"
+          ? callee.fields.map(f => f.type)
+          : callee.type.paramTypes
+      mustHaveCorrectArgumentCount(exps.length, targetTypes.length, { at: open })
+      const args = exps.map((exp, i) => {
+        const arg = exp.rep()
+        mustBeAssignable(arg, { toType: targetTypes[i] }, { at: exp })
+        return arg
+      })
+      return callee?.kind === "StructType"
+        ? core.constructorCall(callee, args)
+        : core.functionCall(callee, args)
+    },
+
+    Primary_id(id) {
+      const entity = context.lookup(id.sourceString)
+      mustHaveBeenFound(entity, id.sourceString, { at: id })
+      return entity
+    },
+
+    true(_) {
+      return true
+    },
+
+    false(_) {
+      return false
+    },
+
+    intlit(_digits) {
+      return BigInt(this.sourceString)
+    },
+
+    floatlit(_whole, _point, _fraction, _e, _sign, _exponent) {
+      return Number(this.sourceString)
+    },
+
+    stringlit(_openQuote, _chars, _closeQuote) {
+      return this.sourceString
+    },
   })
 
   return builder(match).rep()
--- test/analyzer.test.js
+++ test/analyzer.test.js
@@ -3,16 +3,180 @@ import parse from "../src/parser.js"
 import analyze from "../src/analyzer.js"
 import { program, variableDeclaration, variable, binary, floatType } from "../src/core.js"
 
+// Implement indent-specific checks, pipeline checks, and async checks
+
 // Programs that are semantically correct
 const semanticChecks = [
     ["variable declarations", 'int x = 1 \n bool y = "false"'],
-    // ...
+    ["increment and decrement", "int x = 10 \n x-- \n x++",],
+    ["initialize with empty array", "int a = []"],
+    ["assign arrays", "int a = [] \n int b = [1] \n a = b \n b = a"],
+    ["assign to array element", "int a = [1, 2, 3] \n a[1] = 100"],
+    ["short return", "block f(): \n send"],
+    ["long return", "block f() sends boolean: \n send true"],
+    ["return in nested if", "block f(): \n if true: \n send"],
+    ["break in nested if", "while false: \n if true: \n break"],
+    ["long if", "if true: \n say(1) \n else: \n say(3)"],
+    ["elsif", "if true: \n say(1) \n else if true: \n say(0) \n else: say(3)"],
+    ["for over collection", "for i in [2,3,5]: \n say(1)"],
+    ["for in range", "for i in 1..<10: \n say(0)"],
+    ["repeat", "stack 3: \n int a = 1 \n say(a)"],
+    ["||", "say(true || 1 < 2 || false || !true)"],
+    ["&&", "say(true && 1 < 2 && false && !true)"],
+    ["ok to == arrays", "say([1] == [5,8])"],
+    ["ok to != arrays", "print([1] != [5,8])"],
+    ["arithmetic", "int x=1 \n say(2 * 3 + 5 ** -3 / 2 - 5 % 8)"],
+    ["array length", "say([1, 2, 3].length)"],
+  
+    /*
+    // Other tests to modify from Carlos: 
+
+    ["variables", "let x=[[[[1]]]]; print(x[0][0][0][0]+2);"],
+    ["pseudo recursive struct", "struct S {z: S?} let x = S(no S);"],
+    ["nested structs", "struct T{y:int} struct S{z: T} let x=S(T(1)); print(x.z.y);"],
+    ["member exp", "struct S {x: int} let y = S(1);print(y.x);"],
+    ["optional member exp", "struct S {x: int} let y = some S(1);print(y?.x);"],
+    ["subscript exp", "let a=[1,2];print(a[0]);"],
+    ["array of struct", "struct S{} let x=[S(), S()];"],
+    ["struct of arrays and opts", "struct S{x: [int] y: string??}"],
+    ["assigned functions", "function f() {}\nlet g = f;g = f;"],
+    ["call of assigned functions", "function f(x: int) {}\nlet g=f;g(1);"],
+    ["type equivalence of nested arrays", "function f(x: [[int]]) {} print(f([[1],[2]]));"],
+    [
+      "call of assigned function in expression",
+      `function f(x: int, y: boolean): int {}
+      let g = f;
+      print(g(1, true));
+      f = g; // Type check here`,
+    ],
+    [
+      "pass a function to a function",
+      `function f(x: int, y: (boolean)->void): int { return 1; }
+       function g(z: boolean) {}
+       f(2, g);`,
+    ],
+    [
+      "function return types",
+      `function square(x: int): int { return x * x; }
+       function compose(): (int)->int { return square; }`,
+    ],
+    ["function assign", "function f() {} let g = f; let h = [g, f]; print(h[0]());"],
+    ["struct parameters", "struct S {} function f(x: S) {}"],
+    ["array parameters", "function f(x: [int?]) {}"],
+    ["optional parameters", "function f(x: [int], y: string?) {}"],
+    ["empty optional types", "print(no [int]); print(no string);"],
+    ["types in function type", "function f(g: (int?, float)->string) {}"],
+    ["voids in fn type", "function f(g: (void)->void) {}"],
+    ["outer variable", "let x=1; while(false) {print(x);}"],
+    ["built-in constants", "print(25.0 * π);"],
+    ["built-in sin", "print(sin(π));"],
+    ["built-in cos", "print(cos(93.999));"],
+    ["built-in hypot", "print(hypot(-4.0, 3.00001));"],
+
+    // Optional tests: 
+    ["assign optionals", "let a = no int;let b=some 1;a=b;b=a;"],
+    ["conditionals with ints", "print(true ? 8 : 5);"],
+    ["conditionals with floats", "print(1<2 ? 8.0 : -5.22);"],
+    ["conditionals with strings", 'print(1<2 ? "x" : "y");'],
+    ["??", "print(some 5 ?? 0);"],
+    ["nested ??", "print(some 5 ?? 8 ?? 0);"],
+    ["optional types", "let x = no int; x = some 100;"],
+    ["random with array literals, ints", "print(random [1,2,3]);"],
+    ["random with array literals, strings", 'print(random ["a", "b"]);'],
+    ["random on array variables", "let a=[true, false];print(random a);"],
+    */
 ]
 
 // Programs that are syntactically correct but have semantic errors
 const semanticErrors = [
-    ["non-int increment", "bool x= false \n x++", /an integer/],
-    // ...
+    ["non-int increment", "bool x = false \n x++", /an integer/],
+    ["non-int decrement", 'int x = [3, 5] \n x++', /an integer/],
+    ["undeclared id", "say(x);", /Identifier x not declared/],
+    ["redeclared id", "int x = 1 \n int x = 1", /Identifier x already declared/],
+    ["assign to const", "$ x = 1 \n x = 2", /Cannot assign to constant/],
+    ["assign bad type", "int x = 1 \n x = true", /Cannot assign a boolean to a int/],
+    ["assign bad array type", "int x = 1 \n x = [true]", /Cannot assign a \[boolean\] to a int/],
+    ["break outside loop", "break", /Break can only appear in a loop/],
+    ["break inside function", "while true: \n block f(): \n break", /Break can only appear in a loop/],
+    ["return outside function", "send", /Return can only appear in a function/],
+    ["return value from void function","block f(): \n send(1)", /Cannot return a value/,],
+    ["return nothing from non-void", "block f() sends int: \n send", /should be returned/],
+    ["return type mismatch", "block f() sends int: \n send false", /boolean to a int/],
+    ["non-boolean short if test", "if 1: \n say(1)", /Expected a boolean/],
+    ["non-boolean if test", "if 1: \n say(1) \n else \n say(1)", /Expected a boolean/],
+    ["non-boolean while test", "while 1:", /Expected a boolean/],
+    ["non-integer repeat", 'stack "1":', /Expected an integer/],
+    ["non-integer low range", "for i in true...2:", /Expected an integer/],
+    ["non-integer high range", "for i in 1..<3.14:", /Expected an integer/],
+    ["non-array in for", "for i in 100:", /Expected an array/],
+
+    // Other tests to modify from Carlos:
+
+    /*
+    ["bad types for ||", "print(false||1);", /Expected a boolean/],
+    ["bad types for &&", "print(false&&1);", /Expected a boolean/],
+    ["bad types for ==", "print(false==1);", /Operands do not have the same type/],
+    ["bad types for !=", "print(false==1);", /Operands do not have the same type/],
+    ["bad types for +", "print(false+1);", /Expected a number or string/],
+    ["bad types for -", "print(false-1);", /Expected a number/],
+    ["bad types for *", "print(false*1);", /Expected a number/],
+    ["bad types for /", "print(false/1);", /Expected a number/],
+    ["bad types for **", "print(false**1);", /Expected a number/],
+    ["bad types for <", "print(false<1);", /Expected a number or string/],
+    ["bad types for <=", "print(false<=1);", /Expected a number or string/],
+    ["bad types for >", "print(false>1);", /Expected a number or string/],
+    ["bad types for >=", "print(false>=1);", /Expected a number or string/],
+    ["bad types for ==", "print(2==2.0);", /not have the same type/],
+    ["bad types for !=", "print(false!=1);", /not have the same type/],
+    ["bad types for negation", "print(-true);", /Expected a number/],
+    ["bad types for length", "print(#false);", /Expected an array/],
+    ["bad types for not", 'print(!"hello");', /Expected a boolean/],
+    ["bad types for random", "print(random 3);", /Expected an array/],
+    ["non-integer index", "let a=[1];print(a[false]);", /Expected an integer/],
+    ["no such field", "struct S{} let x=S(); print(x.y);", /No such field/],
+    ["diff type array elements", "print([3,3.0]);", /Not all elements have the same type/],
+    ["shadowing", "let x = 1;\nwhile true {let x = 1;}", /Identifier x already declared/],
+    ["call of uncallable", "let x = 1;\nprint(x());", /Call of non-function/],
+    [
+      "Too many args",
+      "function f(x: int) {}\nf(1,2);",
+      /1 argument\(s\) required but 2 passed/,
+    ],
+    [
+      "Too few args",
+      "function f(x: int) {}\nf();",
+      /1 argument\(s\) required but 0 passed/,
+    ],
+    [
+      "Parameter type mismatch",
+      "function f(x: int) {}\nf(false);",
+      /Cannot assign a boolean to a int/,
+    ],
+    [
+      "function type mismatch",
+      `function f(x: int, y: (boolean)->void): int { return 1; }
+       function g(z: boolean): int { return 5; }
+       f(2, g);`,
+      /Cannot assign a \(boolean\)->int to a \(boolean\)->void/,
+    ],
+    ["bad param type in fn assign", "function f(x: int) {} function g(y: float) {} f = g;"],
+    [
+      "bad return type in fn assign",
+      'function f(x: int): int {return 1;} function g(y: int): string {return "uh-oh";} f = g;',
+      /Cannot assign a \(int\)->string to a \(int\)->int/,
+    ],
+    ["bad call to sin()", "print(sin(true));", /Cannot assign a boolean to a float/],
+    ["Non-type in param", "let x=1;function f(y:x){}", /Type expected/],
+    ["Non-type in return type", "let x=1;function f():x{return 1;}", /Type expected/],
+    ["Non-type in field type", "let x=1;struct S {y:x}", /Type expected/],
+
+    // Optional tests: 
+    ["assign bad optional type", "let x=1;x=some 2;", /Cannot assign a int\? to a int/],
+    ["non-boolean conditional test", "print(1?2:3);", /Expected a boolean/],
+    ["diff types in conditional arms", "print(true?1:true);", /not have the same type/],
+    ["unwrap non-optional", "print(1??2);", /Expected an optional/],
+    */
+   
 ]
 
 describe("The analyzer", () => {
