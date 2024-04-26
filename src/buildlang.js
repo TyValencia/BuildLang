2a6c1cc09565f48579949a28957051798d3f5d31
christineli1412 <christineli1412@gmail.com>
2024-04-26 11:28:34 -0700

    add command line script to buidllang.js

--- src/buildlang.js
+++ src/buildlang.js
@@ -1,19 +1,32 @@
-import * as fs from "node:fs/promises"
-import process from "node:process"
-import parse from "./parser.js"
+import * as fs from "node:fs/promises";
+import stringify from "graph-stringify";
+import compile from "./compiler.js";
 
-if (process.argv.length !== 3) {
-  console.log(
-    "Must have exactly one argument: the filename of the program to compile."
-  )
-} else {
+const help = `BuildLang compiler
+
+Syntax: buildlang <filename> <outputType>
+
+Prints to stdout according to <outputType>, which must be one of:
+
+  parsed     a message that the program was matched ok by the grammar
+  analyzed   the statically analyzed representation
+  optimized  the optimized semantically analyzed representation
+  js         the translation to JavaScript
+`;
+
+async function compileFromFile(filename, outputType) {
   try {
-    const buffer = await fs.readFile(process.argv[2])
-    parse(buffer.toString())
-    console.log("Syntax is ok")
+    const buffer = await fs.readFile(filename);
+    const compiled = compile(buffer.toString(), outputType);
+    console.log(stringify(compiled, "kind") || compiled);
   } catch (e) {
-    // log error message in red
-    console.error("\x1b[31m%s\x1b[0m", e.message)
-    process.exit(1)
+    console.error(`\u001b[31m${e}\u001b[39m`);
+    process.exitCode = 1;
   }
+}
+
+if (process.argv.length !== 4) {
+  console.log(help);
+} else {
+  compileFromFile(process.argv[2], process.argv[3]);
 }
